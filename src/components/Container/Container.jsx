f50a7257611846dd9e0429d3c2b5b174b9889d0e
DevGabriel0402 <gabriel.profissional01@gmail.com>
2024-12-31 00:11:47 -0300

    Inserindo as HardSkills

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,10 +1,12 @@
 import "../Container/container.css";
 import { Header } from "../Header/Header.jsx";
+import { Skills } from "../Skills/Skills.jsx";
 
 export const Container = () => {
   return (
     <div className="container">
       <Header />
+      <Skills />
     </div>
   );
 };
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,20 @@
+import "boxicons";
+import "./skills.css";
+
+export const Skills = () => {
+  return (
+    <div className="box-skills">
+      <h2>HardSkills</h2>
+      <div className="skills">
+        <box-icon type="logo" name="html5" color="#fafafa"></box-icon>
+        <box-icon type="logo" name="css3" color="#fafafa"></box-icon>
+        <box-icon type="logo" name="javascript" color="#fafafa"></box-icon>
+        <box-icon type="logo" name="react" color="#fafafa"></box-icon>
+        <box-icon type="logo" name="sass" color="#fafafa"></box-icon>
+        <box-icon type="logo" name="github" color="#fafafa"></box-icon>
+        <box-icon type="logo" name="figma" color="#fafafa"></box-icon>
+        <box-icon type="logo" name="adobe" color="#fafafa"></box-icon>
+      </div>
+    </div>
+  );
+};
