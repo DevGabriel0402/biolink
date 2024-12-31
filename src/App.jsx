1d1db54951ec233cff50f5ca77089df921a5b6ad
DevGabriel0402 <gabriel.profissional01@gmail.com>
2024-12-31 17:42:13 -0300

    Atualizando loading correntamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
 import "./App.css";
-import { useEffect } from "react";
 import { Container } from "./components/Container/Container.jsx";
 
 function App() {
-  useEffect(() => {
-    const hideLoading = () => {
-      const loadingElement = document.getElementById("loading");
-      if (loadingElement) {
-        setTimeout(() => {
-          loadingElement.style.display = "none";
-          loadingElement.style.opacity = "0";
-          loadingElement.style.transition = "all .3s";
-        }, 1200);
-      }
-    };
-
-    window.addEventListener("load", hideLoading);
-  }, []);
   return (
     <>
       <Container />
